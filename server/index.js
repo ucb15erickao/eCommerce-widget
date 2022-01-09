7ffeb9edd4d434f72eb77782c384ea9b32754f97
Eric Kao <68668252+ucb15erickao@users.noreply.github.com>
2022-01-09 02:21:06 -0800

    Delete server directory

--- server/index.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const server = express();
-server.listen(8080, () => {  console.log('express port: 8080');  });
-server.use(express.static(`${__dirname}/../client/dist`));
