9d1730cdffe5692ae69cf712548256ea07f3e1fc
Eric Kao <ucberickao@gmail.com>
2022-01-09 02:42:02 -0800

    Initialize webpack index file

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
 const path = require('path');
 
-const SRC_DIR = path.join(__dirname, '/client/src');
-const DIST_DIR = path.join(__dirname, '/client/dist');
+const SRC_DIR = path.resolve(__dirname, 'client/src');
+const DIST_DIR = path.resolve(__dirname, 'client/dist');
 
 module.exports = {
+  mode: 'production',
   // entry: `${SRC_DIR}/index.jsx`,
-  entry: path.join(__dirname, '/client/src/index.jsx'),
+  entry: path.resolve(__dirname, '/client/src/index.jsx'),
   output: {
     filename: 'bundle.js',
-    // path: DIST_DIR,
-    path: path.join(__dirname, '/client/dist'),
+    path: DIST_DIR,
+    // path: path.join(__dirname, '/client/dist'),
   },
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -18,8 +19,8 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?/,
-        // include: SRC_DIR,
-        include: path.join(__dirname, '/client/src'),
+        include: SRC_DIR,
+        // include: path.resolve(__dirname, '/client/src'),
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
